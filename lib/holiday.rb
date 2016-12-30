5e4b2fb0636be35ac1baa9295a00b91d10facd2b
Jesus Vega <chuyspc@gmail.com>
2016-12-30 03:43:04 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,34 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +56,27 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each_pair do |parent_key,parent_value|
+    #Access Regular keys here
+    puts "#{parent_key.to_s.capitalize!}:"
+    parent_value.each_pair do |child_parent,child_value|
+      #Access the content of the parent key here
+      formated = child_parent.to_s.split("_").map{ |rare_pepe| rare_pepe.capitalize!}
+      puts "  #{formated.join(" ")}: #{child_value.join(", ").to_s}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  includes_bbq = []
+  holiday_hash.each_pair do |parent_key,parent_value|
+    parent_value.each_pair do |child_parent,child_value|
+      includes_bbq << child_parent if child_value.include?("BBQ")
+    end
+  end
+  includes_bbq
 end
 
 
